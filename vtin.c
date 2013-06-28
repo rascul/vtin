be34850e27966d099077b42971dc1dab9a132641
Ray Schulz <ray.schulz42@gmail.com>
2013-06-28 13:32:23 -0400

    some more stuff

--- vtin.c
+++ vtin.c
@@ -1,10 +1,6 @@
 #include <gtk/gtk.h>
 #include <vte/vte.h>
 
-static void print_hello(GtkWidget *widget, gpointer data) {
-	g_print("hello world\n");
-}
-
 int main(int argc, char *argv[]) {
 	GtkWidget *window;
 	GtkWidget *box;
@@ -19,12 +15,32 @@ int main(int argc, char *argv[]) {
 	
 	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 	
+	
+	/* create the terminal widget */
 	char **term_argv = NULL;
 	term = vte_terminal_new();
+	
+	/* tt++ must be in $PATH */
 	g_shell_parse_argv("tt++", NULL, &term_argv, NULL);
 	vte_terminal_fork_command_full(VTE_TERMINAL(term), VTE_PTY_DEFAULT, NULL, term_argv, NULL, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL);
+	
+	/* i would prefer to just disable the damn cursor, but alas, that doesn't seem to be something easy
+	 * valid cursor shapes:
+	 * VTE_CURSOR_SHAPE_IBEAM
+	 * VTE_CURSOR_SHAPE_UNDERLINE
+	 * VTE_CURSOR_SHAPE_BLOCK */
+	vte_terminal_set_cursor_shape(VTE_TERMINAL(term), VTE_CURSOR_SHAPE_IBEAM);
+	
+	/* also, turn off cursor blinking */
+	vte_terminal_set_cursor_blink_mode(VTE_TERMINAL(term), VTE_CURSOR_BLINK_OFF);
+	
+	/* -1 so scrollback buffer will hold everything 
+	 * if memory is an issue, setting this to a finite value (or 0 to disable it) might be useful */
+	vte_terminal_set_scrollback_lines(VTE_TERMINAL(term), -1);
+	
 	gtk_box_pack_start(GTK_BOX(box), term, TRUE, TRUE, 0);
 	
+	/* now we need the input widget */
 	input = gtk_entry_new();
 	gtk_box_pack_start(GTK_BOX(box), input, TRUE, TRUE, 0);
 	
