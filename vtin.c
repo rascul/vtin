f50fef277ad45461721850d223293ff3bd1d466a
Ray Schulz <ray.schulz42@gmail.com>
2013-06-28 13:45:10 -0400

    set default size

--- vtin.c
+++ vtin.c
@@ -10,8 +10,10 @@ int main(int argc, char *argv[]) {
 	
 	gtk_init(&argc, &argv);
 	
+	/* setup main window */
 	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(window), "vtin");
+	gtk_window_set_default_size(GTK_WINDOW(window), 640, 480);
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 	
 	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
